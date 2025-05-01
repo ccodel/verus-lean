bc6bbe5d20847a649650bb59adb04cc62a98ba81
Cayden Codel <ccodel@andrew.cmu.edu>
2025-05-01 13:27:08 -0400

    feat: extract theorems statements in script

--- vl.py
+++ vl.py
@@ -95,6 +95,137 @@ def get_verus_lean_path():
     else:
         return verus_lean_path
 
+# Scans until "MAGIC COMMENT END" is found. If `f` is not None, will print the lines
+# Returns the index of the line after the magic comment
+def scan_for_magic_comment(lines, idx, f):
+    for i in range(idx, len(lines)):
+        if f is not None:
+            f.write(lines[i])
+        if "MAGIC COMMENT END" in lines[i]:
+            return i + 1
+    print("Error: The Lean code does not contain the magic comment end.", file=sys.stderr)
+    sys.exit(1)
+
+
+def get_closing_brace(ch):
+    if ch == "(":
+        return ")"
+    elif ch == "{":
+        return "}"
+    elif ch == "⦃":
+        return "⦄"
+    else:
+        print("Error: The provided character is not a valid opening brace:", ch, file=sys.stderr)
+        sys.exit(1)
+
+
+# TODO: Make this not recursive
+# TODO: Don't strip whitespace?
+# Scans the lines until the matching closing brace is found
+# Returns a pair of the line and char index of the char after the closing brace
+def skip_to_closing_brace(lines, line_idx, char_idx, brace):
+    for line_idx in range(line_idx, len(lines)):
+        line = lines[line_idx].strip()
+        for i in range(char_idx, len(line)):
+            ch = line[i]
+            if ch == brace:
+                return (line_idx, i + 1)
+            elif ch in "({⦃":
+                (new_line_idx, new_char_idx) = skip_to_closing_brace(lines, line_idx, i + 1, get_closing_brace(ch))
+                i = new_char_idx
+                if line_idx != new_line_idx:
+                    line_idx = new_line_idx
+                    line = lines[line_idx].strip()
+    
+    print("Error: No matching closing brace found.", file=sys.stderr)
+    sys.exit(1)
+
+def skip_to_next_non_whitespace(lines, line_idx, char_idx):
+    for line_idx in range(line_idx, len(lines)):
+        line = lines[line_idx].strip()
+        for i in range(char_idx, len(line)):
+            ch = line[i]
+            if ch != " ":
+                return (line_idx, i)
+        char_idx = 0
+    
+    print("Error: No non-whitespace character found.", file=sys.stderr)
+    sys.exit(1)
+
+# Returns the theorem statement until, but not including, the ending `:=`,
+# ignoring new lines and double spaces (substituting them for a single space)
+# Returns a pair of (name, statement, start_idx)
+# TODO: Currently assuming that this section ends with `MAGIC COMMENT END`
+# TODO: Assumes that the user does not use `:=` or braces in "evil" ways
+def collect_theorem_statement(lines, line_idx):
+    name = None
+    statement = None
+    start_idx = None
+
+    # Find the start of the theorem
+    for i in range(line_idx, len(lines)):
+        # Assume that the theorem starts with `theorem`
+        # TODO: @simp annotations?
+        line = lines[i].strip()
+        if line.startswith("theorem"):
+            # TODO Assume that the theorem name immediately follows "theorem"
+            name_start = 8   # 8 == len("theorem ")
+            name_end = -1
+            start_idx = i
+            for j in range(name_start, len(line) + 1):
+                if j == len(line) or line[j] == " ":
+                    name_end = j
+                    name = line[name_start:name_end]
+                    break
+            break
+
+    # Skip ahead
+    (line_idx, char_idx) = skip_to_next_non_whitespace(lines, i, name_end)
+
+    # Now accumulate the theorem statement
+    statement_start_line_idx = line_idx
+    statement_start_char_idx = char_idx
+
+    for i in range(line_idx, len(lines)):
+        line = lines[i].strip()
+        for j in range(char_idx, len(line)):
+            char_idx = 0
+            ch = line[j]
+            if ch in "({⦃":
+                (new_i, j) = skip_to_closing_brace(lines, i, j + 1, get_closing_brace(ch))
+                if i != new_i:
+                    line = lines[new_i].strip()
+                    i = new_i
+            elif j < len(line) - 1 and line[j] == ":" and line[j + 1] == "=":
+                # We found the end of the theorem statement
+                # Re-construct it
+                statement = None
+                if i == statement_start_line_idx:
+                    statement = lines[i].strip()[statement_start_char_idx:(j - 1)].strip()
+                else:
+                    statement = lines[statement_start_line_idx].strip()[statement_start_char_idx:]
+                    for k in range(statement_start_line_idx + 1, i):
+                        statement += " " + lines[k].strip()
+                    statement += " " + lines[i].strip()[:j - 1].strip()
+                
+                return (name, statement, start_idx)
+
+    print("Error: No matching `:=` found for the theorem statement.", file=sys.stderr)
+    sys.exit(1)
+
+# Returns (line_idx, statements), where `line_idx` is one more than `MAGIC COMMENT END`
+def collect_theorem_statements(lines, line_idx):
+    statements = []
+    while line_idx < len(lines) - 1 and "MAGIC COMMENT END" not in lines[line_idx + 1]:
+        line = lines[line_idx].strip()
+        if len(lines) == 0:
+            continue
+        elif line.startswith("theorem"):
+            thm = collect_theorem_statement(lines, line_idx)
+            statements.append(thm)
+        line_idx += 1
+
+    return (line_idx + 2, statements)
 
 ################################################################################
 # Main execution
@@ -154,10 +285,9 @@ print()
 #    Skip the rest of the pipeline if the hash is the same
 verus_base_name = os.path.basename(verus_file).split(".")[0]
 serialized_file = os.path.join(cwd, f"serialized_{verus_base_name}.json")
-hash_file_path = os.path.join(cwd, f"serialized_{verus_base_name}.sha256")
-serialization_existed = os.path.exists(serialized_file)
+serialization_existed = os.path.exists(serialized_file) and os.access(serialized_file, os.R_OK)
 
-# (Re-)generate the serialization. Then compare the hashes
+# (Re-)generate the serialization
 verus_result = subprocess.run([verus_binary, verus_file], capture_output=True, text=True)
 if verus_result.returncode != 0:
     print("Error: verus failed to run successfully. Below is its error output:", file=sys.stderr)
@@ -167,33 +297,7 @@ if verus_result.returncode != 0:
 # The serialized file should now be in our current directory
 check_for_valid_file(serialized_file, os.R_OK)
 
-# Capture the SHA256 hash
-hash_result = subprocess.run(["sha256sum", serialized_file], capture_output=True, text=True)
-hash_value = hash_result.stdout.split()[0]
-
-# 2. Write down a hash, or compare hashes to skip Lean code generation
-if not serialization_existed:
-    print("Success! verus serialized the file to:", serialized_file)
-    with open(hash_file_path, "w") as hash_file:
-        hash_file.write(hash_value + "\n")
-else:
-    # There should be a hash for us to compare against
-    # Check if the hash is the same
-    check_for_valid_file(hash_file_path, os.R_OK)
-    old_hash_value = ""
-    with open(hash_file_path, "r") as hash_file:
-        old_hash_value = hash_file.readline().strip()
-
-    if hash_value == old_hash_value:
-        print("The hashes of the serialization files matched. Skipping Lean code generation.")
-        print("Success!")
-        sys.exit(0)
-    else:
-        # Update the hash file and continue with the script
-        with open(hash_file_path, "w") as hash_file:
-            hash_file.write(hash_value + "\n")
-
-# 3. Generate or update the Lean file
+# 2. Generate or update the Lean file
 if not lean_file_exists:
     # If the Lean file doesn't exist, run `verus-lean` directly
     lean_result = subprocess.run([verus_lean_binary, serialized_file, lean_file], capture_output=True, text=True)
@@ -205,7 +309,6 @@ if not lean_file_exists:
         print("Success! verus-lean generated the Lean file")
 else:
     # The Lean file already exists - update the definitions
-    # TODO HERE
     check_for_valid_file(lean_file, os.R_OK)
     print("The Lean file already exists. Overwriting the definitions...")
 
@@ -215,11 +318,11 @@ else:
 
     # Pipe the new Lean generation to stdout
     # For now, assume that generation succeeded
-    # TODO: As of 4/30, `Main.lean` catches all errors, so return code will always be 0
+    # TODO: As of 4/30, `Main.lean` catches all errors, so the return code will always be 0
     #       As a workaround, we check if the first line of output is an import statement
     lean_result = subprocess.run([verus_lean_binary, serialized_file], capture_output=True, text=True)
     if lean_result.returncode != 0 or not "import VerusLean.Basic" in lean_result.stdout:
-        print("Error: verus-lean failed to run successfully. Below is its error output:", file=sys.stderr)
+        print("Error: verus-lean failed. Below is its error output:", file=sys.stderr)
         print(lean_result.stderr, file=sys.stderr)
         sys.exit(1)
     new_lines = lean_result.stdout.splitlines(keepends=True)
@@ -227,46 +330,47 @@ else:
     # Erase previous contents
     f = open(lean_file, "w")
 
-    # Scan forward for "MAGIC COMMENT END", writing previous contents line by line
-    magic_index = -1
-    for i in range(len(lines)):
-        f.write(lines[i])
-        if "MAGIC COMMENT END" in lines[i]:
-            # The comment ends three lines after this one
-            f.write(lines[i + 1])
-            f.write(lines[i + 2])
-            f.write(lines[i + 3])
-            magic_index = i + 3
-            break
+    # -> "Start section (1)"
+    magic_index = scan_for_magic_comment(lines, 0, f)
+    new_magic_index = scan_for_magic_comment(new_lines, 0, None)
 
-    if magic_index == -1:
-        print("Error: The Lean file does not contain the magic comment end.", file=sys.stderr)
-        sys.exit(1)
+    # -> "End section (1)"
+    # Write new output over the old data structures section
+    magic_index = scan_for_magic_comment(lines, magic_index, None)
+    new_magic_index = scan_for_magic_comment(new_lines, new_magic_index, f)
 
-    # TODO More intelligently replace things
-    new_magic_index = -1
-    for i in range(len(new_lines)):
-        if "MAGIC COMMENT END" in new_lines[i]:
-            new_magic_index = i + 3
-            break
+    # -> "Start section (2)"
+    # Write intervening code until user theorem statement section
+    magic_index = scan_for_magic_comment(lines, magic_index, f)
+    new_magic_index = scan_for_magic_comment(new_lines, new_magic_index, None)
 
-    # TODO just write the output until the second magic end
-    for i in range(new_magic_index, len(new_lines)):
-        f.write(new_lines[i])
-        if "MAGIC COMMENT END" in new_lines[i]:
-            # The comment ends three lines after this one
-            f.write(new_lines[i + 1])
-            f.write(new_lines[i + 2])
-            f.write(new_lines[i + 3])
-            break
+    # -> "End section (2)"
+    # Leave the old user theorem statements alone
+    # TODO: Add missing/new theorem names
 
-    # Scan forward for the ending "MAGIC COMMENT END"
-    for i in range(magic_index, len(lines)):
-        if "MAGIC COMMENT END" in lines[i]:
-            # The comment ends three lines after this one
-            magic_index = i + 3
-            break
+    (e, thms) = collect_theorem_statements(lines, magic_index)
+    print("old theorem statements")
+    for (name, statement, start_idx) in thms:
+        print(f"theorem {name} {statement}")
 
+    print("newly generated statements")
+    (e_new, thms) = collect_theorem_statements(new_lines, new_magic_index)
+    for (name, statement, start_idx) in thms:
+        print(f"theorem {name} {statement}")
+
+    magic_index = scan_for_magic_comment(lines, magic_index, f)
+    new_magic_index = scan_for_magic_comment(new_lines, new_magic_index, None)
+
+    # -> "Start section (3)"
+    magic_index = scan_for_magic_comment(lines, magic_index, f)
+    new_magic_index = scan_for_magic_comment(new_lines, new_magic_index, None)
+
+    # -> "End section (3)"
+    # Write the new user theorem statements
+    magic_index = scan_for_magic_comment(lines, magic_index, None)
+    new_magic_index = scan_for_magic_comment(new_lines, new_magic_index, f)
+
+    # Write the rest of the old file
     for i in range(magic_index, len(lines)):
         f.write(lines[i])
 
